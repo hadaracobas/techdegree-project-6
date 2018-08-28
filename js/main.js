2bc8ef33a1c767c0b5d1e4b00fbb3a52ae45eac0
Hadar Acobas <hadarleviacobas@gmail.com>
2018-08-28 12:14:35 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,43 @@
+
+// video media player
+$('video').mediaelementplayer({
+  features: ['playpause', 'tracks', 'progress', 'fullscreen', 'volume',],
+  startLanguage: 'en',
+  stretching: 'responsive'
+});
+
+
+// Highlight Text
+const v = document.getElementsByTagName('video')[0];
+const spanText = document.querySelectorAll('p span');
+
+v.ontimeupdate = () => {
+  const vTime = v.currentTime;
+
+  for(let i = 0; i < spanText.length; i += 1) {
+    if(vTime < spanText[i].getAttribute('data-end') && vTime > spanText[i].getAttribute('data-start')) {
+      spanText[i].style.color = "#F15025"
+    } else{
+      spanText[i].style.color = "#101935"
+    }
+  }
+};
+
+
+
+
+// play the clicked point
+for(let i = 0; i < spanText.length; i += 1) {
+  spanText[i].addEventListener('click', (event) => {
+    v.currentTime = event.target.getAttribute('data-start');
+    v.play();
+  })
+}
+
+
+// pointer
+for(let i = 0; i < spanText.length; i += 1) {
+  spanText[i].addEventListener('mouseover', (event) => {
+    spanText[i].style.cursor = "pointer";
+  })
+}
